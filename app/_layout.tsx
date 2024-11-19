3272e3fb0374b301429bc9c4d339e36d56e2b2ff
Ilir Bajrami <ilirfreelancer@gmail.com>
2024-11-19 12:55:41 +0100

    to reproduce2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,13 @@
-import "react-native-reanimated";
-import "../global.css";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (loaded) {
-    SplashScreen.hideAsync();
-  }
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <Stack>
-          <Stack.Screen name='index' options={{ headerShown: false }} />
-          <Stack.Screen name='(protected)' options={{ headerShown: false }} />
-        </Stack>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='(protected)' options={{ headerShown: false }} />
+    </Stack>
   );
 }
